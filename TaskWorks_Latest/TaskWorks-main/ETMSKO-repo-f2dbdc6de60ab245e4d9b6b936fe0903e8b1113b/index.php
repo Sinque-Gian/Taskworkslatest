a630a38b4410b785ec337f76dd00efda33e9507f
Sinque-Gian <144778533+Sinque-Gian@users.noreply.github.com>
2024-06-10 22:56:52 +0800

    Add files via upload

---
+++ TaskWorks_Latest/TaskWorks-main/ETMSKO-repo-f2dbdc6de60ab245e4d9b6b936fe0903e8b1113b/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="media/etmsko-icon.png" type="image/x-icon">
+    <script src="includes/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.js">
+    <link rel="stylesheet" href="css/style.css">
+    <title>TasksWorks</title>
+
+    
+</head>
+<body id="index">
+
+    <section>
+        <h1 id="logintitle">TaskWorks</h1>
+        <h5 id="loginsubhead">Where tasks works for you</h5>
+    </section>
+    <div class="row">
+        <div class="col-md-4" id="home-page">
+            <center><h3 class="text-balck">Choose login mode</h3></center>
+            <hr>
+            <br>
+            
+            <a href="user_login.php" class="btn  " style="margin-right: 20px; background-color:#EDB5BF; font-weight: bold;">Student Login</a>
+            <a href="registration.php" class="btn " style="margin-right: 20px; background-color:#EDB5BF; font-weight: bold;">Student Registration</a>
+            <a href="admin/admin_login.php" class="btn " style="background-color:#EDB5BF; font-weight: bold;">Teacher Login</a>
+
+            <br>
+            <img src="media/usericon.png" alt="usericon" style="padding-top: 20px;">
+        </div>
+    </div>
+</body>
+</html>
